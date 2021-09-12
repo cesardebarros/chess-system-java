408359389f4fdd90752b13c73abfb800b0b8145a
cesardebarros <hhhcesar@gmail.com>
2021-09-12 16:40:19 -0300

    Chess exception and ChessPosition

--- src/application/UI.java
+++ src/application/UI.java
@@ -6,6 +6,7 @@ public class UI {
 
 	public static void printBoard(ChessPiece[][] pieces) {
 		
+		
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
@@ -13,7 +14,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h ");
+		System.out.println("  A B C D E F G H ");
 	}
 	
 	private static void printPiece(ChessPiece piece) {
